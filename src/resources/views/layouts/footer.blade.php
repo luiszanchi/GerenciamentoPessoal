29bdbdbf4e6c9838c67e621bf6a4299360a4ebe8
Luis Zanchi <lftzanchi@gmail.com>
2018-11-21 10:14:13 -0200

    Added and configured Laravel with Auth, Added AdminLte2 in public

---
+++ src/resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<div class="pull-right hidden-xs">
+      <b>Version</b> 2.4.0
+    </div>
+    <strong>Copyright &copy; 2014-2016 <a href="{{Config::get('app.url')}}/assets/https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+    reserved.
